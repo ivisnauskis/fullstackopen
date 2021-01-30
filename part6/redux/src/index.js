e9d8dbac80afc85719e5c409f9a3e9bebbec7e15
ivisnauskis <invisnauskis@gmail.com>
2021-01-30 12:26:30 +0200

    redux notes

--- part6/redux/src/index.js
+++ part6/redux/src/index.js
@@ -1,8 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
-const App = () => {
-  return <div>Hello</div>;
+const noteReducer = (state = [], action) => {
+  if (action.type === "NEW_NOTE") {
+    return state.concat(action.data);
+  }
+
+  return state;
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "thep app state is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2,
+  },
+});
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? "important" : ""}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+renderApp();
+store.subscribe(renderApp);
