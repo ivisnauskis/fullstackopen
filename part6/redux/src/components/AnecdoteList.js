a1b48257f49c965a169e9850bbdbdb86460a006d
ivisnauskis <invisnauskis@gmail.com>
2021-01-31 21:31:40 +0200

    6.12

--- part6/redux/src/components/AnecdoteList.js
+++ part6/redux/src/components/AnecdoteList.js
@@ -7,6 +7,13 @@ const AnecdoteList = () => {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
 
+  const filter = state.filter;
+
+  const anecdotes = state.anecdotes.filter((a) =>
+    a.content.toUpperCase().includes(filter.toUpperCase())
+  );
+
+  console.log("filtered", anecdotes);
   const vote = (id, content) => {
     dispatch(voteFor(id));
     dispatch(setNotification(`You voted '${content}'`));
@@ -14,7 +21,7 @@ const AnecdoteList = () => {
 
   return (
     <div>
-      {state.anecdotes.map((anecdote) => (
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
--- part6/redux/src/store.js
+++ part6/redux/src/store.js
@@ -2,10 +2,12 @@ import anecdoteReducer from "./reducers/anecdoteReducer";
 import notificationReducer from "./reducers/notificationReducer";
 import { createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import filterReducer from "./reducers/filterReducer";
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
   notification: notificationReducer,
+  filter: filterReducer,
 });
 
 const store = createStore(reducer, composeWithDevTools());
