6e8c121333984c8829ed6307903b983780df2617
ivisnauskis <invisnauskis@gmail.com>
2021-01-31 10:31:52 +0200

    anecdotes redux

--- part6/redux/src/index.js
+++ part6/redux/src/index.js
@@ -1,8 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
 
-const App = () => {
-  return <div>Hello</div>;
-};
+const store = createStore(reducer)
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
